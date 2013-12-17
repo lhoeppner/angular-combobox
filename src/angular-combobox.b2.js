c3d14bd8fda132d638dc6f9ebb315708b052f15e
lhoeppner <lars.hoeppner@gmail.com>
2013-12-17 07:44:28 +0100

    refactor close ddl

--- src/angular-combobox.b2.js
+++ src/angular-combobox.b2.js
@@ -1,32 +1,63 @@
 /**
  * TODO:
- * - select on click
- * - remove select() and only use "=" for selected
+ * - allow multiple select
+ * - keyboard navigation of items (up/down/return)
+ * - on click outside, clear filter term
+ * - height option (make scrollable inside)
  * - on focus input, clear filter text (only display selected in non-focused)
  * - multiple breaks...also breaks if we require ^ngModel ?
+ * - test with themes (fix .select esp)
  *
  * @type {*}
  */
 var combobox = angular.module('ui.angular-combobox', []);
 
-combobox.directive('combobox', function ($document, $location, $log, $filter) {
+combobox.factory('safeApply', [function ($rootScope) {
+    return function ($scope, fn) {
+        var phase = $scope.$root.$$phase;
+        if (phase == '$apply' || phase == '$digest') {
+            if (fn) {
+                $scope.$eval(fn);
+            }
+        } else {
+            if (fn) {
+                $scope.$apply(fn);
+            } else {
+                $scope.$apply();
+            }
+        }
+    }
+}]);
+
+combobox.directive('combobox', function ($document, $location, $log, $filter, safeApply) {
         var openElement = null,
             closeMenu = angular.noop,
             COMBOBOX_ID = "_combobox_id";
 
-        var inputEl = angular.element('<input type="text" ng-model="state.searchTerm" ng-keydown="onKeyDown"' +
+        var inputEl = angular.element('<input type="text" ng-model="state.searchTerm" ng-keydown="onKeyDown" clear-on-focus ' +
             'style="margin-bottom: 0px; width:{{style.inputWidth}}px"/>');
         var dropdownEl = angular.element('<ul class="dropdown-menu" style="width:{{style.width}}px">' +
-            '<li ng-class="{selected: isSelected(choice)}" ng-repeat="choice in filteredItems">' +
-            '<a>{{choice.text}}</a>' +
+            '<li ng-class="{selected: isSelected(item)}" ng-repeat="item in filteredItems" ng-click="selectItem(item)">' +
+            '<a>{{item.text}}</a>' +
             '</li>' +
             '</ul>');
 
         var multiSelectDropdownEl = angular.element('<ul class="dropdown-menu" style="width:{{style.width}}px">' +
-            '<li ng-repeat="choice in filteredItems">' +
-            '<input type="checkbox"/>   <a>{{choice}}</a>' +
+            '<li ng-repeat="item in filteredItems">' +
+            '<input type="checkbox"/>   <a>{{item}}</a>' +
             '</li>' +
             '</ul>');
+
+//        var safeApply = function(fn) {
+//            var phase = this.$root.$$phase;
+//            if(phase == '$apply' || phase == '$digest') {
+//                if(fn && (typeof(fn) === 'function')) {
+//                    fn();
+//                }
+//            } else {
+//                this.$apply(fn);
+//            }
+//        };
 
         var link = function (scope, element, attrs) {
             console.log(scope)
@@ -38,13 +69,21 @@ combobox.directive('combobox', function ($document, $location, $log, $filter) {
                 inputWidth: width - 12 // adjust for input's padding (2*6px padding, might want to get actual values dynamically for easier maintenance)
             };
 
-            scope.state = {
-                searchTerm: "",
-                selectedIndex: -1
+            scope.getSearchTerm = function () {
+                return scope.state.searchTerm || "";
+            };
+
+            scope.setSearchTerm = function (value) {
+                scope.state.searchTerm = value;
             };
 
             scope.clearFilter = function () {
-                scope.filteredItems = $filter('filter')(scope.pristineItems, "");
+                safeApply(scope, function () { // TODO why do I need to $apply here?
+                    inputEl.val("");
+                    scope.setSearchTerm("");
+                    scope.filteredItems = $filter('filter')(scope.pristineItems, "");
+                });
+
             };
 
             scope.pristineItems = []; //angular.extend([], scope.items);
@@ -57,41 +96,42 @@ combobox.directive('combobox', function ($document, $location, $log, $filter) {
             }
             scope.clearFilter(); // initialize filtered items
 
+            console.log("passed state");
+            console.log(scope.state);
+
             scope.getSelectedIndex = function () {
                 return scope.state.selectedIndex;
             };
 
-            scope.setSelectedIndex = function (value) {
-                scope.state.selectedIndex = value;
-            };
 
-            scope.getSearchTerm = function () {
-                return scope.state.searchTerm || "";
-            };
-
-            scope.setSearchTerm = function (value) {
-                scope.state.searchTerm = value;
-            };
-
-            scope.selectElement = function (toSelect, preventClose) {
+            scope.selectItem = function (toSelect, preventClose) {
                 // TODO handle if multiselect..
-                scope.selectedElements = [toSelect];
-                scope.setSelectedIndex(toSelect[COMBOBOX_ID]);
+//                scope.selectedElements = [toSelect];
 
+                console.log("selecting");
+                console.log(toSelect);
+
+                //noinspection JSUnresolvedFunction
                 inputEl.val(toSelect.text);
-                scope.$apply(function () {
+
+                safeApply(scope, function () {
                     scope.clearFilter();
-                    scope.selectedText = toSelect.text; // TODO
-                    scope.select({text: toSelect.text}); // TODO fix this
+                    scope.state._selectedItem = toSelect; // store internal type with generated id
+                    scope.state.selectedItem = toSelect.text;
+                    scope.state.selectedIndex = toSelect[COMBOBOX_ID];
+                    //scope.select({text: toSelect.text}); // TODO fix this
                 });
 
                 // TODO pass preventClose in multiselect case
                 if (!preventClose) {
-                    closeMenu();
+                    if (!!openElement) {
+                        closeMenu();
+                    }
                 }
             };
 
             scope.isSelected = function (item) {
+
                 return item[COMBOBOX_ID] === scope.getSelectedIndex();
             };
 
@@ -99,7 +139,7 @@ combobox.directive('combobox', function ($document, $location, $log, $filter) {
                 return toTest.text.indexOf(value) !== -1;
             };
 
-// TODO allow custom keydown event handler
+            // TODO allow custom keydown event handler
             scope.onKeyDown = function ($event) {
                 console.log(scope);
                 if ($event.which == 9) {
@@ -112,11 +152,10 @@ combobox.directive('combobox', function ($document, $location, $log, $filter) {
                     var matches = $filter('filter')(items, value, filterFunction);
                     if (matches.length) {
                         var toSelect = matches[0];
-                        scope.selectElement(toSelect);
+                        scope.selectItem(toSelect);
                     } else {
-                        // clear filter ?
+                        closeMenu();
                     }
-
                 }
             };
 
@@ -138,33 +177,41 @@ combobox.directive('combobox', function ($document, $location, $log, $filter) {
                 closeMenu();
             });
 
-            element.bind('click', function (event) {
+            scope.openMenu = function () {
+                element.addClass('open');
+                openElement = dropdownEl;
+                closeMenu = function (event) {
+                    if (event) {
+                        event.preventDefault();
+                        event.stopPropagation();
+                    }
+                    $document.unbind('click', closeMenu);
+                    element.removeClass('open');
+                    closeMenu = angular.noop;
+                    openElement = null;
+
+                    scope.clearFilter();
+                };
+
+                $document.bind('click', closeMenu);
+            };
+
+            inputEl.bind('click', function (event) {
                 $log.log("click");
                 $log.log(event);
 
-                var elementWasOpen = (element === openElement);
 
                 event.preventDefault();
                 event.stopPropagation();
+
+                var elementWasOpen = (element === openElement);
 
                 if (!!openElement) {
                     closeMenu();
                 }
 
                 if (!elementWasOpen && !element.hasClass('disabled') && !element.prop('disabled')) {
-                    element.addClass('open');
-                    openElement = element;
-                    closeMenu = function (event) {
-                        if (event) {
-                            event.preventDefault();
-                            event.stopPropagation();
-                        }
-                        $document.unbind('click', closeMenu);
-                        element.removeClass('open');
-                        closeMenu = angular.noop;
-                        openElement = null;
-                    };
-                    $document.bind('click', closeMenu);
+                    scope.openMenu();
                 }
             });
         };
@@ -173,8 +220,8 @@ combobox.directive('combobox', function ($document, $location, $log, $filter) {
             restrict: 'A',
             replace: true,
             scope: {
-                select: "&",
-                comboboxItems: "=" // TODO why doesn't this work?
+                state: "=",
+                comboboxItems: "="
             },
             template: '<div class="dropdown"/>',
             compile: function (templateEl, attributes) {
@@ -193,6 +240,17 @@ combobox.directive('combobox', function ($document, $location, $log, $filter) {
         };
     }
 );
+
+combobox.directive('clearOnFocus', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, elem) {
+            elem.bind('focus', function () {
+                elem.val("");
+            });
+        }
+    };
+});
 
 combobox.directive('ngKeydown', function () {
     return {
